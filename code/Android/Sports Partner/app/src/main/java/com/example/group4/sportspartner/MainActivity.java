af581cce83784b680503213e462802fb587d30c3
Xuan Zhang <xuanzhang@jhu.edu>
2017-10-11 17:27:24 -0400

    add sign up page and corresponding button activities

--- code/Android/Sports Partner/app/src/main/java/com/example/group4/sportspartner/MainActivity.java
+++ code/Android/Sports Partner/app/src/main/java/com/example/group4/sportspartner/MainActivity.java
@@ -36,4 +36,77 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    public void signup(View v){
+
+        setContentView(R.layout.signup_activity);
+
+    }
+    public void signupIndividual(View v){
+        EditText userNameField = (EditText)findViewById(R.id.userName);
+        EditText passwordField = (EditText)findViewById(R.id.password);
+        EditText confirmPasswordField = (EditText)findViewById(R.id.confirmPassword);
+        EditText emailField = (EditText)findViewById(R.id.email);
+
+        String userName = userNameField.getText().toString();
+        String password = passwordField.getText().toString();
+        String confirmPassword = confirmPasswordField.getText().toString();
+        String email = emailField.getText().toString();
+
+        Context context = getApplicationContext();
+        CharSequence text = "";
+        int duration = Toast.LENGTH_SHORT;
+
+
+        if(userName.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || email.isEmpty()){
+            text = "missing info";
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else if(!password.equals(confirmPassword)){
+            text = "wrong password confirmation";
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else{
+            text = "success";
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            setContentView(R.layout.static_main_page);
+        }
+
+    }
+    public void signupProvider(View v){
+        EditText userNameField = (EditText)findViewById(R.id.userName);
+        EditText passwordField = (EditText)findViewById(R.id.password);
+        EditText confirmPasswordField = (EditText)findViewById(R.id.confirmPassword);
+        EditText emailField = (EditText)findViewById(R.id.email);
+
+        String userName = userNameField.getText().toString();
+        String password = passwordField.getText().toString();
+        String confirmPassword = confirmPasswordField.getText().toString();
+        String email = emailField.getText().toString();
+
+        Context context = getApplicationContext();
+        CharSequence text = "";
+        int duration = Toast.LENGTH_SHORT;
+
+
+        if(userName.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || email.isEmpty()){
+            text = "missing info";
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else if(!password.equals(confirmPassword)){
+            text = "wrong password confirmation";
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else{
+            text = "success";
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            setContentView(R.layout.static_main_page);
+        }
+
+    }
 }
