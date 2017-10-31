3d27b37cb6de6a64d88c79c8d51ad4269d9ee9b4
Yujia Xiao <yxiao15@jhu.edu>
2017-10-30 20:19:57 -0400

    Android: Xmls and related Activites updated

---
+++ code/Android/Sports Partner/app/src/main/java/com/sportspartner/activity/SactivityDetailActivity.java
@@ -0,0 +1,27 @@
+package com.sportspartner.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sportspartner.R;
+
+public class SactivityDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ViewGroup content = (ViewGroup) findViewById(R.id.layout_home);
+        getLayoutInflater().inflate(R.layout.activity_sdetail, content, true);
+
+        //set title of toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Activity Details");
+
+    }
+
+}
---
+++ code/Android/Sports Partner/app/src/main/java/com/sportspartner/activity/SignupActivity.java
@@ -0,0 +1,104 @@
+package com.sportspartner.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.sportspartner.R;
+import com.sportspartner.service.UserService;
+import com.sportspartner.util.ActivityCallBack;
+import com.sportspartner.service.serviceresult.BooleanResult;
+
+public class SignupActivity extends AppCompatActivity {
+    EditText emailField;
+    EditText passwordField;
+    EditText confirmPasswordField;
+    String email;
+    String password;
+    String confirmPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        emailField = (EditText)findViewById(R.id.signUpEmail);
+        passwordField = (EditText)findViewById(R.id.password);
+        confirmPasswordField = (EditText)findViewById(R.id.confirmPassword);
+    }
+
+    /**
+     * Onclick listener for "sign up as indivadual" button
+     * @param v
+     */
+    public void signupIndividual(View v){
+        signupUser("person");
+    }
+
+    /**
+     * Onclick listener for "sign up as facility provider" button
+     * @param v
+     */
+    public void signupProvider(View v){
+        signupUser("facilityprovider");
+    }
+
+
+    public void backToLogin(View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void signupUser(String type) {
+        email = emailField.getText().toString();
+        password = passwordField.getText().toString();
+        confirmPassword = confirmPasswordField.getText().toString();
+
+        if(password.isEmpty() || confirmPassword.isEmpty() || email.isEmpty()){
+            Context context = this.getApplicationContext();
+            Toast toast = Toast.makeText(context, "missing field", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+        if (!password.equals(confirmPassword)) {
+            Context context = this.getApplicationContext();
+            Toast toast = Toast.makeText(context, "confirm password doesn't match", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+
+        UserService.signup(this, email, password, confirmPassword, type, new ActivityCallBack(){
+            @Override
+            public void onSuccess(BooleanResult signupResult) {
+                signupHandler(signupResult);
+            }
+        });
+    }
+    private void signupHandler(BooleanResult signupResult) {
+        // handle the result here
+        String message = signupResult.getMessage();
+        if (message != null) {
+            Toast toast = Toast.makeText(SignupActivity.this, message, Toast.LENGTH_LONG);
+            toast.show();
+        }
+        if (signupResult.isStatus()) {
+            Toast toast = Toast.makeText(SignupActivity.this, "sign up successfully!", Toast.LENGTH_LONG);
+            toast.show();
+            UserService.login(this, email, password, new ActivityCallBack(){
+                @Override
+                public void onSuccess(BooleanResult booleanResult) {
+                    // TODO link to the main page activity of this user
+                    Context context = getApplicationContext();
+                    Intent intent = new Intent(context, ProfileActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+}
