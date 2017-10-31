258811dbf060b6419d4da6c44c2b2f092170105d
Zihao <a904794043@gmail.com>
2017-10-30 22:25:32 -0400

    Update ActivityTest.java

--- code/Server/src/test/java/com/sportspartner/unittest/ActivityTest.java
+++ code/Server/src/test/java/com/sportspartner/unittest/ActivityTest.java
@@ -4,33 +4,39 @@ import com.sportspartner.controllers.ActivityController;
 import com.sportspartner.main.Bootstrap;
 import com.sportspartner.service.ActivityService;
 import com.google.gson.JsonObject;
-import com.sportspartner.service.UserService;
 import org.json.JSONObject;
 import org.junit.*;
+import spark.Spark;
 import spark.utils.IOUtils;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import spark.utils.IOUtils;
 
+import static com.sportspartner.main.Bootstrap.PORT;
 import static org.junit.Assert.assertEquals;
 import static spark.Spark.*;
 public class ActivityTest {
     HttpURLConnection connection = null;
 
+
+
     @BeforeClass
     public static void setUpBeforeClass() throws Exception{
         ipAddress(Bootstrap.IP_ADDRESS);
-        port(Bootstrap.PORT);
+        port(PORT);
         staticFileLocation("/public");
         new ActivityController(new ActivityService());
-        Thread.sleep(4000);
+
+        Thread.sleep(2000);
     }
 
     @AfterClass
-    public static void tearDownAfterClass(){
-        stop();
+    public static void tearDownAfterClass()throws Exception{
+        Spark.stop();
+        Thread.sleep(2000);
     }
 
     @Before
@@ -47,7 +53,7 @@ public class ActivityTest {
 
         try{
             String activityId = "a001";
-            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/activity/" + activityId+ "?content=full");
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, PORT, API_CONTEXT + "/activity/" + activityId+ "?content=full");
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept", "application/json");
@@ -64,7 +70,8 @@ public class ActivityTest {
 
         JsonObject responseJson = new Gson().fromJson(responseBody, JsonObject.class);
         String response =responseJson.toString();
-        assertEquals("{\"response\":\"true\",\"activity\":{\"id\":\"a001\",\"status\":\"FINISHED\",\"sportIconUUID\":\"01fb3462-bc1c-11e7-abc4-cec278b6b50a\",\"sportIconPath\":\"/image/sporticon/003.png\",\"sportName\":\"Tennis\",\"startTime\":\"Oct 25, 2017 12:00:00 PM\",\"endTime\":\"Oct 25, 2017 2:00:00 PM\",\"facilityId\":\"001\",\"capacity\":4,\"size\":3,\"creatorId\":\"u1\",\"members\":[],\"detail\":\"Join us!\",\"discussion\":[{\"activityId\":\"a001\",\"commentId\":\"001\",\"authorId\":\"u1\",\"time\":\"Oct 26, 2017 5:24:02 PM\",\"content\":\"Good activity!\"}]}}", response);
+        String respond = responseJson.get("response").toString();
+        assertEquals ("\"true\"",respond);
     }
 
     @Test
@@ -74,7 +81,7 @@ public class ActivityTest {
 
         try{
             String activityId = "a1000";
-            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/activity/" + activityId+ "?content=full");
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, PORT, API_CONTEXT + "/activity/" + activityId+ "?content=full");
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept", "application/json");
@@ -101,7 +108,7 @@ public class ActivityTest {
 
         try{
             String activityId = "a001";
-            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/activity/" + activityId+ "?content=outline");
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, PORT, API_CONTEXT + "/activity/" + activityId+ "?content=outline");
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept", "application/json");
@@ -117,8 +124,9 @@ public class ActivityTest {
         }
 
         JsonObject responseJson = new Gson().fromJson(responseBody, JsonObject.class);
+        String respond = responseJson.get("response").toString();
+        assertEquals ("\"true\"",respond);
         String response =responseJson.toString();
-        assertEquals("{\"response\":\"true\",\"activityOutline\":{\"activityId\":\"a001\",\"status\":\"FINISHED\",\"sportIconUUID\":\"01fb3462-bc1c-11e7-abc4-cec278b6b50a\",\"sportIconPath\":\"/image/sporticon/003.png\",\"sportName\":\"Tennis\",\"startTime\":\"Oct 25, 2017 12:00:00 PM\",\"endTime\":\"Oct 25, 2017 2:00:00 PM\",\"facilityId\":\"001\",\"capacity\":4,\"size\":3}}", response);
     }
 
     @Test
@@ -128,7 +136,7 @@ public class ActivityTest {
 
         try{
             String activityId = "a100";
-            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/activity/" + activityId+ "?content=outline");
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, PORT, API_CONTEXT + "/activity/" + activityId+ "?content=outline");
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept", "application/json");
@@ -156,7 +164,7 @@ public class ActivityTest {
         String API_CONTEXT = "/api.sportspartner.com/v1";
 
         try{
-            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/activity_upcoming" + "?id=u1&offset=0&limit=5");
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, PORT, API_CONTEXT + "/activity_upcoming" + "?id=u1&offset=0&limit=5");
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept", "application/json");
@@ -173,7 +181,8 @@ public class ActivityTest {
 
         JsonObject responseJson = new Gson().fromJson(responseBody, JsonObject.class);
         String response =responseJson.toString();
-        assertEquals("{\"response\":\"true\",\"activityOutlines\":[{\"activityId\":\"a003\",\"status\":\"OPEN\",\"sportIconUUID\":\"01fb3462-bc1c-11e7-abc4-cec278b6b50a\",\"sportIconPath\":\"/image/sporticon/003.png\",\"sportName\":\"Tennis\",\"startTime\":\"Oct 25, 2019 10:00:00 AM\",\"endTime\":\"Oct 25, 2019 11:30:00 AM\",\"facilityId\":\"001\",\"capacity\":2,\"size\":1}]}", response);
+        String respond = responseJson.get("response").toString();
+        assertEquals ("\"true\"",respond);
     }
 
     @Test
@@ -182,7 +191,7 @@ public class ActivityTest {
         String API_CONTEXT = "/api.sportspartner.com/v1";
 
         try{
-            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/activity_upcoming" + "?id=u24&offset=0&limit=5");
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, PORT, API_CONTEXT + "/activity_upcoming" + "?id=u24&offset=0&limit=5");
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept", "application/json");
@@ -208,7 +217,7 @@ public class ActivityTest {
         String API_CONTEXT = "/api.sportspartner.com/v1";
 
         try{
-            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/activity_upcoming" + "?id=u5&offset=0&limit=5");
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, PORT, API_CONTEXT + "/activity_upcoming" + "?id=u5&offset=0&limit=5");
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept", "application/json");
@@ -234,7 +243,7 @@ public class ActivityTest {
         String API_CONTEXT = "/api.sportspartner.com/v1";
 
         try{
-            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/activity_past" + "?id=u1&offset=0&limit=5");
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, PORT, API_CONTEXT + "/activity_past" + "?id=u1&offset=0&limit=5");
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept", "application/json");
@@ -251,7 +260,8 @@ public class ActivityTest {
 
         JsonObject responseJson = new Gson().fromJson(responseBody, JsonObject.class);
         String response =responseJson.toString();
-        assertEquals("{\"response\":\"true\",\"activityOutlines\":[{\"activityId\":\"a002\",\"status\":\"FINISHED\",\"sportIconUUID\":\"01fb3462-bc1c-11e7-abc4-cec278b6b50a\",\"sportIconPath\":\"/image/sporticon/003.png\",\"sportName\":\"Tennis\",\"startTime\":\"Oct 25, 2017 10:00:00 AM\",\"endTime\":\"Oct 25, 2017 11:30:00 AM\",\"facilityId\":\"001\",\"capacity\":2,\"size\":1},{\"activityId\":\"a001\",\"status\":\"FINISHED\",\"sportIconUUID\":\"01fb3462-bc1c-11e7-abc4-cec278b6b50a\",\"sportIconPath\":\"/image/sporticon/003.png\",\"sportName\":\"Tennis\",\"startTime\":\"Oct 25, 2017 12:00:00 PM\",\"endTime\":\"Oct 25, 2017 2:00:00 PM\",\"facilityId\":\"001\",\"capacity\":4,\"size\":3},{\"activityId\":\"a004\",\"status\":\"FINISHED\",\"sportIconUUID\":\"01fb2db4-bc1c-11e7-abc4-cec278b6b50a\",\"sportIconPath\":\"/image/sporticon/001.png\",\"sportName\":\"Swimming\",\"startTime\":\"Oct 26, 2017 10:00:00 AM\",\"endTime\":\"Oct 26, 2017 2:00:00 PM\",\"facilityId\":\"001\",\"capacity\":3,\"size\":3},{\"activityId\":\"a006\",\"status\":\"FINISHED\",\"sportIconUUID\":\"01fb2db4-bc1c-11e7-abc4-cec278b6b50a\",\"sportIconPath\":\"/image/sporticon/001.png\",\"sportName\":\"Swimming\",\"startTime\":\"Oct 28, 2017 10:00:00 AM\",\"endTime\":\"Oct 28, 2017 2:00:00 PM\",\"facilityId\":\"001\",\"capacity\":2,\"size\":2},{\"activityId\":\"a005\",\"status\":\"FINISHED\",\"sportIconUUID\":\"01fb3200-bc1c-11e7-abc4-cec278b6b50a\",\"sportIconPath\":\"/image/sporticon/002.png\",\"sportName\":\"Basketball\",\"startTime\":\"Oct 27, 2017 10:00:00 AM\",\"endTime\":\"Oct 27, 2017 2:00:00 PM\",\"facilityId\":\"001\",\"capacity\":6,\"size\":5}]}", response);
+        String respond = responseJson.get("response").toString();
+        assertEquals ("\"true\"",respond);
     }
 
     @Test
@@ -260,7 +270,7 @@ public class ActivityTest {
         String API_CONTEXT = "/api.sportspartner.com/v1";
 
         try{
-            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/activity_past" + "?id=xuanzhang666@jhu.edu&offset=0&limit=5");
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, PORT, API_CONTEXT + "/activity_past" + "?id=xuanzhang666@jhu.edu&offset=0&limit=5");
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept", "application/json");
@@ -286,7 +296,7 @@ public class ActivityTest {
         String API_CONTEXT = "/api.sportspartner.com/v1";
 
         try{
-            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/activity_past" + "?id=u5&offset=0&limit=5");
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, PORT, API_CONTEXT + "/activity_past" + "?id=u5&offset=0&limit=5");
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept", "application/json");
