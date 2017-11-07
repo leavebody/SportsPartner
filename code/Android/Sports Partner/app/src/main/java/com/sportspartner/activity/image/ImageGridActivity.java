9d4ca555000688d602ff834934bfea652728889a
Xiaochen Li <leave.body@hotmail.com>
2017-11-07 16:05:59 -0500

    commit to pull

---
+++ code/Android/Sports Partner/app/src/main/java/com/sportspartner/activity/image/ImageGridActivity.java
@@ -0,0 +1,12 @@
+package com.sportspartner.activity.image;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImageGridActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
