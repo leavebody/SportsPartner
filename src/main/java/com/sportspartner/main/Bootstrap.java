88bac8bc8df215b9ea2932302fa0b6bce03622e8
Ized06 <a904794043@gmail.com>
2017-11-12 12:10:23 -0500

    1

--- src/main/java/com/sportspartner/main/Bootstrap.java
+++ src/main/java/com/sportspartner/main/Bootstrap.java
@@ -23,7 +23,7 @@ import static spark.Spark.*;
 
 
 public class Bootstrap {
-    public static final String IP_ADDRESS = "localhost";
+    public static final String IP_ADDRESS = "0.0.0.0";
     public static final int PORT = 8080;
 
     private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
