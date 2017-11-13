70f7b5019f403dce9ee0ea5db912ed2f8b5448b0
Ized06 <a904794043@gmail.com>
2017-11-12 20:54:12 -0500

    ?

--- src/main/java/com/sportspartner/util/ConnectionUtil.java
+++ src/main/java/com/sportspartner/util/ConnectionUtil.java
@@ -29,7 +29,13 @@ public class ConnectionUtil {
 //            c = DriverManager
 //                    .getConnection("jdbc:postgresql://elmer.db.elephantsql.com:5432/rdkxzlvf", "rdkxzlvf", "At7YAFMgJqq1aMAcqMTY9CixdC_toDeM");
             //c = DriverManager.getConnection("postgres://npvilmribuqyfn:4de73b009926d90511554c0cd40ca9fa0a133b604d768fbc4abffe1d00cff4fb@ec2-50-17-217-166.compute-1.amazonaws.com:5432/ddv402olp7iu27", "npvilmribuqyfn","4de73b009926d90511554c0cd40ca9fa0a133b604d768fbc4abffe1d00cff4fb");
-            c = DriverManager.getConnection("postgresql://" + host + ":" + port + path, username, password);
+            System.out.println(host);
+            System.out.println(port);
+            System.out.println(path);
+            System.out.println(username);
+            System.out.println(password);
+            
+            c = DriverManager.getConnection("postgres://" + host + ":" + port + path, username, password);
         } catch ( Exception e ) {
             System.err.println( "nao ni mei"+e.getClass().getName()+": "+ e.getMessage() );
             System.exit(0);
