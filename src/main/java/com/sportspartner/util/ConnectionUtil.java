a83060991fa91de1b0861f6845536a80b163d30f
Ized06 <a904794043@gmail.com>
2017-11-13 09:23:34 -0500

    ?

--- src/main/java/com/sportspartner/util/ConnectionUtil.java
+++ src/main/java/com/sportspartner/util/ConnectionUtil.java
@@ -26,8 +26,7 @@ public class ConnectionUtil {
             String path = dbUri.getPath();
             String username = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[0];
             String password = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[1];
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://elmer.db.elephantsql.com:5432/rdkxzlvf", "rdkxzlvf", "At7YAFMgJqq1aMAcqMTY9CixdC_toDeM");
+
             //c = DriverManager.getConnection("postgres://npvilmribuqyfn:4de73b009926d90511554c0cd40ca9fa0a133b604d768fbc4abffe1d00cff4fb@ec2-50-17-217-166.compute-1.amazonaws.com:5432/ddv402olp7iu27", "npvilmribuqyfn","4de73b009926d90511554c0cd40ca9fa0a133b604d768fbc4abffe1d00cff4fb");
             System.out.println(host);
             System.out.println(port);
@@ -43,6 +42,8 @@ public class ConnectionUtil {
 
             //c = DriverManager.getConnection("jdbc:postgresql://" + host + ":" + port + path, username, password);
             //c =  DriverManager.getConnection(dbUrl);
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://elmer.db.elephantsql.com:5432/rdkxzlvf", "rdkxzlvf", "At7YAFMgJqq1aMAcqMTY9CixdC_toDeM");
         } catch ( Exception e ) {
             System.err.println(e.getClass().getName()+": "+ e.getMessage() );
             System.exit(0);
