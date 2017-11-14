972e6a53a7644e56c8c0c3005eed8d2cdbc1b382
Ized06 <a904794043@gmail.com>
2017-11-14 09:39:19 -0500

    try postgre database

--- src/main/java/com/sportspartner/util/ConnectionUtil.java
+++ src/main/java/com/sportspartner/util/ConnectionUtil.java
@@ -17,34 +17,28 @@ public class ConnectionUtil {
         Connection c = null;
         try {
               Class.forName("org.postgresql.Driver");
-//            if (System.getenv("DATABASE_URL") == null) {
-//                dbUri = new URI("postgres://localhost:5432/to_do");
-//            } else {
-//                dbUri = new URI(System.getenv("DATABASE_URL"));
-//            }
-//            int port = dbUri.getPort();
-//            String host = dbUri.getHost();
-//            String path = dbUri.getPath();
-//            String username = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[0];
-//            String password = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[1];
-//
-//            //c = DriverManager.getConnection("postgres://npvilmribuqyfn:4de73b009926d90511554c0cd40ca9fa0a133b604d768fbc4abffe1d00cff4fb@ec2-50-17-217-166.compute-1.amazonaws.com:5432/ddv402olp7iu27", "npvilmribuqyfn","4de73b009926d90511554c0cd40ca9fa0a133b604d768fbc4abffe1d00cff4fb");
-//            System.out.println(host);
-//            System.out.println(port);
-//            System.out.println(path);
-//            System.out.println(username);
-//            System.out.println(password);
-//
-//            String un = dbUri.getUserInfo().split(":")[0];
-//            String pw = dbUri.getUserInfo().split(":")[1];
-//            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/to_do");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[1];
 
-            //c =  DriverManager.getConnection(dbUrl, un, pw);
+            //c = DriverManager.getConnection("postgres://npvilmribuqyfn:4de73b009926d90511554c0cd40ca9fa0a133b604d768fbc4abffe1d00cff4fb@ec2-50-17-217-166.compute-1.amazonaws.com:5432/ddv402olp7iu27", "npvilmribuqyfn","4de73b009926d90511554c0cd40ca9fa0a133b604d768fbc4abffe1d00cff4fb");
+            System.out.println(host);
+            System.out.println(port);
+            System.out.println(path);
+            System.out.println(username);
+            System.out.println(password);
 
-            //c = DriverManager.getConnection("jdbc:postgresql://" + host + ":" + port + path, username, password);
+            c = DriverManager.getConnection("jdbc:postgresql://" + host + ":" + port + path, username, password);
             //c =  DriverManager.getConnection(dbUrl);
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://elmer.db.elephantsql.com:5432/rdkxzlvf", "rdkxzlvf", "At7YAFMgJqq1aMAcqMTY9CixdC_toDeM");
+            //c = DriverManager
+            //        .getConnection("jdbc:postgresql://elmer.db.elephantsql.com:5432/rdkxzlvf", "rdkxzlvf", "At7YAFMgJqq1aMAcqMTY9CixdC_toDeM");
         } catch ( Exception e ) {
             System.err.println(e.getClass().getName()+": "+ e.getMessage() );
             System.exit(0);
