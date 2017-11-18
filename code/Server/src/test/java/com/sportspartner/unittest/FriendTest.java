64a70d1d3da6aa5f57facc99ac5ffb7c87baa4da
Ized06 <a904794043@gmail.com>
2017-11-17 22:36:09 -0500

    add test cases

---
+++ code/Server/src/test/java/com/sportspartner/unittest/FriendTest.java
@@ -0,0 +1,124 @@
+package com.sportspartner.unittest;
+
+import com.sportspartner.controllers.FriendController;
+import com.sportspartner.dao.impl.FriendDaoImpl;
+import com.sportspartner.main.Bootstrap;
+import com.sportspartner.service.FriendService;
+import org.junit.*;
+import spark.Spark;
+import spark.utils.IOUtils;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+import static spark.Spark.ipAddress;
+import static spark.Spark.port;
+import static spark.Spark.staticFileLocation;
+
+public class FriendTest {
+
+    HttpURLConnection connection = null;
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception{
+        ipAddress(Bootstrap.IP_ADDRESS);
+        port(Bootstrap.PORT);
+        staticFileLocation("/public");
+        new FriendController(new FriendService());
+        Thread.sleep(2000);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception{
+
+        new FriendDaoImpl().newFriend("u1","u3");
+        Spark.stop();
+        Thread.sleep(2000);
+    }
+
+    @Before
+    public void setUp(){}
+
+    @After
+    public void teardown(){
+
+    }
+
+    @Test
+    public void getFriendSuccess(){
+        String responseBody = new String();
+        String API_CONTEXT = "/api.sportspartner.com/v1";
+        String userId = "u1";
+
+        try{
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/"+userId+"/friends" );
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoOutput(true);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        try{
+            responseBody = IOUtils.toString(connection.getInputStream());
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+
+        assertEquals("{\"response\":\"true\",\"friendlist\":[{\"userId\":\"xuanzhang@jhu.edu\",\"userName\":\"Xuan Zhang\",\"iconUUID\":\"20e7d49f-5bb6-431e-a511-bc5e0edb349f\"},{\"userId\":\"u4\",\"userName\":\"Lion\",\"iconUUID\":\"80629b4b-884a-4b66-b397-f05f9b503157\"},{\"userId\":\"zxiao10@jhu.edu\",\"userName\":\"Zihao Xiao\",\"iconUUID\":\"007\"},{\"userId\":\"leavebody@hotmail.com\",\"userName\":\"Xiaochen Li\",\"iconUUID\":\"92cf9134-e40b-40f5-baf3-f7d9990a61bf\"},{\"userId\":\"shirish@gmail.com\",\"userName\":\"Shirish\",\"iconUUID\":\"ad1ff689-613d-45a0-a51c-485556f69751\"},{\"userId\":\"u3\",\"userName\":\"Tiger\",\"iconUUID\":\"95a67fc1-5bb3-458a-bad5-4dd7314cd310\"}]}", responseBody);
+    }
+
+    @Test
+    public void deleteFriendSuccess(){
+        String responseBody = new String();
+        String API_CONTEXT = "/api.sportspartner.com/v1";
+        String userId1 = "u1";
+        String userId2 = "u3";
+
+        try{
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/deletefriend/"+userId1+"/"+userId2 );
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("DELETE");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoOutput(true);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        try{
+            responseBody = IOUtils.toString(connection.getInputStream());
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+
+        assertEquals("{\"response\":\"true\"}", responseBody);
+    }
+    @Test
+    public void deleteFriendFailure(){
+        String responseBody = new String();
+        String API_CONTEXT = "/api.sportspartner.com/v1";
+        String userId1 = "u4";
+        String userId2 = "u3";
+
+        try{
+            URL url = new URL("http", Bootstrap.IP_ADDRESS, Bootstrap.PORT, API_CONTEXT + "/deletefriend/"+userId1+"/"+userId2 );
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("DELETE");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoOutput(true);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        try{
+            responseBody = IOUtils.toString(connection.getInputStream());
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+
+        assertEquals("{\"response\":\"false\",\"message\":\"They are not friends at all!\"}", responseBody);
+    }
+
+}
