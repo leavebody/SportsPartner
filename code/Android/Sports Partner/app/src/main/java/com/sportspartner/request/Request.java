21bcc8c47c75a1d1fdc8f731f042628cce19ef9a
Yujia Xiao <yxiao15@jhu.edu>
2017-12-13 15:48:05 -0500

    Andorid edit refresh

--- code/Android/Sports Partner/app/src/main/java/com/sportspartner/request/Request.java
+++ code/Android/Sports Partner/app/src/main/java/com/sportspartner/request/Request.java
@@ -17,13 +17,6 @@ public class Request {
     //for emulator
     //protected static final String URL_CONTEXT = "http://10.0.2.2:4567/api.sportspartner.com/";
 
-    //server: heroku
-    protected static final String URL_CONTEXT = "http://oosesportspartner.herokuapp.com/api.sportspartner.com/";
-    //server: localhost, device: tablet
-    //protected static final String URL_CONTEXT = "http://10.194.79.55:4567/api.sportspartner.com/";
-    //server: localhost, device: emulator
-    //protected static final String URL_CONTEXT = "http://10.0.2.2:4567/api.sportspartner.com/";
-
     protected Context contextf;
 
     public Request(Context c){
