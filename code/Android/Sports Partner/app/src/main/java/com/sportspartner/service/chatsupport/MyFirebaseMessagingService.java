b73a56138dc9172a72c05921561b974447279585
Ized06 <a904794043@gmail.com>
2017-12-13 17:29:50 -0500

    notification problem

--- code/Android/Sports Partner/app/src/main/java/com/sportspartner/service/chatsupport/MyFirebaseMessagingService.java
+++ code/Android/Sports Partner/app/src/main/java/com/sportspartner/service/chatsupport/MyFirebaseMessagingService.java
@@ -45,44 +45,44 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
     // [START receive_message]
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        // [START_EXCLUDE]
-        // There are two types of messages data messages and notification messages. Data messages are handled
-        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
-        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
-        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
-        // When the user taps on the notification they are returned to the app. Messages containing both notification
-        // and data payloads are treated as notification messages. The Firebase console always sends notification
-        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-
-        String channelUrl = null;
-        try {
-            JSONObject sendBird = new JSONObject(remoteMessage.getData().get("sendbird"));
-            JSONObject channel = (JSONObject) sendBird.get("channel");
-            channelUrl = (String) channel.get("channel_url");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-        sendNotification(this, remoteMessage.getData().get("message"), channelUrl);
-    }
+//    @Override
+//    public void onMessageReceived(RemoteMessage remoteMessage) {
+//        // [START_EXCLUDE]
+//        // There are two types of messages data messages and notification messages. Data messages are handled
+//        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+//        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+//        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+//        // When the user taps on the notification they are returned to the app. Messages containing both notification
+//        // and data payloads are treated as notification messages. The Firebase console always sends notification
+//        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+//        // [END_EXCLUDE]
+//
+//        // TODO(developer): Handle FCM messages here.
+//        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+//        Log.d(TAG, "From: " + remoteMessage.getFrom());
+//
+//        // Check if message contains a data payload.
+//        if (remoteMessage.getData().size() > 0) {
+//            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+//        }
+//
+//        // Check if message contains a notification payload.
+//        if (remoteMessage.getNotification() != null) {
+//            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+//        }
+//
+//        String channelUrl = null;
+//        try {
+//            JSONObject sendBird = new JSONObject(remoteMessage.getData().get("sendbird"));
+//            JSONObject channel = (JSONObject) sendBird.get("channel");
+//            channelUrl = (String) channel.get("channel_url");
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        // Also if you intend on generating your own notifications as a result of a received FCM
+//        // message, here is where that should be initiated. See sendNotification method below.
+//        sendNotification(this, remoteMessage.getData().get("message"), channelUrl);
+//    }
     // [END receive_message]
 
     /**
