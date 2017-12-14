76ae31d63076be94008ae09d8aec4ab0b1e347d1
Ized06 <a904794043@gmail.com>
2017-12-14 17:53:12 -0500

    add null judgement

--- code/Server/src/main/java/com/sportspartner/util/DaoUtil.java
+++ code/Server/src/main/java/com/sportspartner/util/DaoUtil.java
@@ -5,17 +5,33 @@ import java.sql.*;
 public class DaoUtil {
 
     public static void CloseDao (ResultSet rs, PreparedStatement stmt, Connection c)throws SQLException{
+        if(rs!=null) {
             rs.close();
+        }
+        if(stmt!=null) {
             stmt.close();
+        }
+        if(c!=null) {
             c.close();
+        }
     }
     public static void CloseDaoNoRs (PreparedStatement stmt, Connection c)throws SQLException{
-        stmt.close();
-        c.close();
+        if(stmt!=null) {
+            stmt.close();
+        }
+        if(c!=null) {
+            c.close();
+        }
     }
     public static void CloseDao (ResultSet rs, Statement stmt, Connection c)throws SQLException{
-        rs.close();
-        stmt.close();
-        c.close();
+        if(rs!=null) {
+            rs.close();
+        }
+        if(stmt!=null) {
+            stmt.close();
+        }
+        if(c!=null) {
+            c.close();
+        }
     }
 }
